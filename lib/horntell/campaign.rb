bbe075c3c03be28691fc4efa2dbf860211958b80
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 08:22:39 +0000

    fixed campaign namespace

--- lib/horntell/campaign.rb
+++ lib/horntell/campaign.rb
@@ -1,17 +1,15 @@
 module Horntell
-	module Ruby
-		class Campaign
-			extend Horntell::Ruby::Http::ApiRequester
+	class Campaign
+		extend Horntell::Ruby::Http::ApiRequester
 
-			def self.to_profile(uid, campaignId)
-				return raw_request('post', "/profiles/#{uid}/campaigns/#{campaignId}")
-			end
+		def self.to_profile(uid, campaignId)
+			return raw_request('post', "/profiles/#{uid}/campaigns/#{campaignId}")
+		end
 
-			def self.to_profiles(profiles, campaignId)
-				profiles = { :profile_uids => profiles }
+		def self.to_profiles(profiles, campaignId)
+			profiles = { :profile_uids => profiles }
 
-				return raw_request('post', "/profiles/campaigns/#{campaignId}", profiles)
-			end
+			return raw_request('post', "/profiles/campaigns/#{campaignId}", profiles)
 		end
 	end
 end
