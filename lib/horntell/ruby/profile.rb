8d4bccfe595b8a586f09b5df6956f91748f5de83
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 05:56:42 +0000

    added the profile class

---
+++ lib/horntell/ruby/profile.rb
@@ -0,0 +1,23 @@
+module Horntell
+	module Ruby
+		class Profile
+			extend Horntell::Ruby::Http::ApiRequester
+
+			def self.find(uid)
+				return raw_request('get', "/profiles/#{uid}")
+			end
+
+			def self.create(profile)
+				return raw_request('post', "/profiles", profile)
+			end
+
+			def self.update(uid, profile)
+				return raw_request('put', "/profiles/#{uid}", profile)
+			end
+
+			def self.delete(uid)
+				return raw_request('delete', "/profiles/#{uid}")
+			end
+		end
+	end
+end
