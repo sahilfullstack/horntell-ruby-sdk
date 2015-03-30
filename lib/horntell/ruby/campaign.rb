d58efd2e03cedce5428aa04405357620ff4a8b38
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 05:59:22 +0000

    added the campaign class

---
+++ lib/horntell/ruby/campaign.rb
@@ -0,0 +1,17 @@
+module Horntell
+	module Ruby
+		class Campaign
+			extend Horntell::Ruby::Http::ApiRequester
+
+			def self.to_profile(uid, campaignId)
+				return raw_request('post', "/profiles/#{uid}/campaigns/#{campaignId}")
+			end
+
+			def self.to_profiles(profiles, campaignId)
+				profiles = { :profile_uids => profiles }
+
+				return raw_request('post', "/profiles/campaigns/#{campaignId}", profiles)
+			end
+		end
+	end
+end
