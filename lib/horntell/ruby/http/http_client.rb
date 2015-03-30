fac1d7c105cbd7b8b7ae07bc3848d429b49e8019
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 06:51:30 +0000

    Added http client

---
+++ lib/horntell/ruby/http/http_client.rb
@@ -0,0 +1,81 @@
+module Horntell
+	module Ruby
+		module Http
+			module HttpClient
+				def request(method, url, headers=nil, params=nil, username, password)
+					begin
+						opts = {
+							:method => method,
+							:headers => headers,
+							:url => url,
+							:payload => params,
+							:user => username,
+							:password => password,
+							:timeout => 80
+						}
+
+
+						response = RestClient::Request.execute(opts)
+
+						return response
+					rescue RestClient::ExceptionWithResponse => e
+						if code = e.http_code and body = e.http_body
+							return handle_api_error(code, body)
+						else
+							raise Horntell::Ruby::NetworkError.new
+						end
+					rescue Exception => e
+						raise Horntell::Ruby::NetworkError.new
+					end
+				end
+			def handle_api_error(code, body)
+				begin
+					error_obj = JSON.parse(body)
+
+					error = error_obj["error"]
+				end
+
+
+				case code
+					when 400
+						raise invalid_request_error error, code
+					when 401
+						raise authentication_error error, code
+					when 403
+						raise forbidden_error error, code
+					when 404
+						raise not_found_error error, code
+					when 500
+						raise service_error error, code
+					else
+						raise horntell_error
+					end
+				end
+
+				def authentication_error(error, code)
+					AuthenticationError.new(error["message"], code, error["type"])
+				end
+
+				def forbidden_error(error, code)
+					ForbiddenError.new(error["message"], code, error["type"])
+				end
+
+				def horntell_error(error, code)
+					HorntellError.new(error["message"], code, error["type"])
+				end
+
+				def invalid_request_error(error, code)
+					InvalidRequestError.new(error["message"], code, error["type"])
+				end
+
+				def not_found_error(error, code)
+					NotFoundError.new(error["message"], code, error["type"])
+				end
+
+				def service_error(error, code)
+					ServiceError.new(error["message"], code, error["type"])
+				end
+			end
+		end
+	end
+end
