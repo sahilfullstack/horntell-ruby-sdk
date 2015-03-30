c3b22f309f3b40630396c6f8648aae76dea7f14c
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 08:25:31 +0000

    fixed namespace of response class

--- lib/horntell/http/response.rb
+++ lib/horntell/http/response.rb
@@ -1,27 +1,25 @@
 module Horntell
-	module Ruby
-		module Http
-			class Response
-				attr_reader :response
-				attr_reader :body
-				attr_reader :code
+	module Http
+		class Response
+			attr_reader :response
+			attr_reader :body
+			attr_reader :code
 
-				def initialize(response=nil)
-					@response = response
-					@body = get_code == 204 ? nil : JSON.parse(body)
-				end
+			def initialize(response=nil)
+				@response = response
+				@body = get_code == 204 ? nil : JSON.parse(body)
+			end
 
-				def get_original
-					return response
-				end
+			def get_original
+				return response
+			end
 
-				def get_body
-					return body
-				end
+			def get_body
+				return body
+			end
 
-				def get_code
-					return response.code
-				end
+			def get_code
+				return response.code
 			end
 		end
 	end
