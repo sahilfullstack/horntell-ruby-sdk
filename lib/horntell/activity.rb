2795abcef83f14e993d38864331a3bd3bef37a86
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 09:42:25 +0000

    removed activity.rb

--- lib/horntell/activity.rb
+++
@@ -1,9 +0,0 @@
-module Horntell
-	class Activity
-		extend Horntell::Http::ApiRequester
-
-		def self.create(uid, activity)
-			return raw_request('post', "/profiles/#{uid}/activities", activity)
-		end
-	end
-end
