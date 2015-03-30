d89d50af2d2a49595ed626644f1ff39233684f1c
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-30 08:23:47 +0000

    fixed activity namespace

--- lib/horntell/activity.rb
+++ lib/horntell/activity.rb
@@ -1,11 +1,9 @@
 module Horntell
-	module Ruby
-		class Activity
-			extend Horntell::Ruby::Http::ApiRequester
+	class Activity
+		extend Horntell::Http::ApiRequester
 
-			def self.create(uid, activity)
-				return raw_request('post', "/profiles/#{uid}/activities", activity)
-			end
+		def self.create(uid, activity)
+			return raw_request('post', "/profiles/#{uid}/activities", activity)
 		end
 	end
 end
